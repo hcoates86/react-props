db1c0c67603bc80c35ab752801806c46cff898bf
hcoates <115696699+hcoates86@users.noreply.github.com>
2023-04-25 19:46:08 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import BaseStats from './BaseStats';
 import './App.css';
 
 
-function App() {
+function App(stats) {
+  const baseStats = {
+  hp: 45,
+  attack: 49,
+  defense: 49,
+  spAttack: 65,
+  spDef: 65,
+  speed: 45,
+};
   return (
     <div className='main-wrapper background'>
       <Showcase />
-      <BaseStats />
+      <BaseStats stats={baseStats} />
     </div>
+    
   );
 }
 
